e47c46522e32b5223e417ad1ceddad48ae4f48b4
liushuai <liu3399shuai@163.com>
2018-03-28 17:49:07 +0800

    change

--- module_login/LoginViewController.h
+++ module_login/LoginViewController.h
@@ -1,10 +1,3 @@
-//
-//  LoginViewController.h
-//  test_common
-//
-//  Created by liu on 2018/3/28.
-//  Copyright © 2018年 liu. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 
--- module_login/LoginViewController.m
+++ module_login/LoginViewController.m
@@ -1,10 +1,3 @@
-//
-//  LoginViewController.m
-//  test_common
-//
-//  Created by liu on 2018/3/28.
-//  Copyright © 2018年 liu. All rights reserved.
-//
 
 #import "LoginViewController.h"
 
@@ -16,22 +9,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
